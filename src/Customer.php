c93ca8ef55757ae2c436facce892af713c8f7b96
naazeeeeh <88356407+naazeeeeh@users.noreply.github.com>
2021-08-03 10:02:06 +0700

    Add value

--- src/Customer.php
+++ src/Customer.php
@@ -8,7 +8,7 @@ class Customer {
         
     }
 
-    public function sayHello(string $name = "guest") : string
+    public function sayHello(string $name = "Guest") : string
     {
         return "Hello $name, My Name is $this->name";
     }
